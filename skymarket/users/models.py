724969d9de516c21b32640a74d753218518e4e8f
Daniyar Batyrbaev <batyrbaev_daniyar@mail.ru>
2024-04-27 22:12:38 +0500

    fix: modules refactoring

--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractBaseUser, AbstractUser
 from django.db import models
 from users.managers import UserManager
-from phonenumber_field.modelfields import PhoneNumberField
+# from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
 
 
